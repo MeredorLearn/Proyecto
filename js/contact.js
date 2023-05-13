db94a80443afe9dc6f5990a24465ccd8d514c84a
MeredorLearn <pablorqlmlearn@gmail.com>
2023-05-13 19:22:51 -0300

    contact

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+formulario = document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_seq10ob';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Mensaje enviado!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
